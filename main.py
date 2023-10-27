72b55df858052279849d545f5ced18730cee793c
Abhishek.H <abhishek18h@gmail.com>
2023-10-27 18:12:12 +0530

    T-T

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from asyncore import loop
 from ursina import *
 from ursina import Audio
 from ursina.prefabs.first_person_controller import FirstPersonController
+from perlin_noise import PerlinNoise
+import random
+
+noise = PerlinNoise(octaves=3,seed=random.randint(1,1000))
 
 app = Ursina()
 audio = Audio()
@@ -12,12 +16,13 @@ brick_texture = load_texture('assets/brick_block.png')
 dirt_texture = load_texture('assets/dirt_block.png')
 sky_texture = load_texture('assets/skybox.png')
 arm_texture = load_texture('assets/arm_texture.png')
+sand_texture = load_texture('assets/sand_block.png')
 
-# placing_sound = audio.create_sound('assets/block_place.wav')
-# breaking_sound = audio.create_sound('assets/bone_break.wav')
+placing_sound = Audio('assets/block_place.wav')
+breaking_sound = Audio('assets/bone_break.wav')
 
 
-block_pick = 1
+block_pick = grass_texture
 
 window.fps_counter.enabled = False
 window.exit_button.visible = False
@@ -27,28 +32,29 @@ initial_player_position = (10, 1, 10)  # Adjust the position as needed
 
 player = FirstPersonController(position=initial_player_position)
 
-def update():
+def update_block_pick():
     global block_pick
-    player_y = player.y
-
-    if player_y < -35:  # Check if the player falls below the threshold
-    # Teleport the player back to the platform
-            player.position = initial_player_position
+    if held_keys['1']:
+        block_pick = grass_texture
+    if held_keys['2']:
+        block_pick = stone_texture
+    if held_keys['3']:
+        block_pick = brick_texture
+    if held_keys['4']:
+        block_pick = dirt_texture
+    if held_keys['5']:
+        block_pick = sand_texture
+    
+def update():
+    if player.y < -35:
+        player.position = (0, 1, 0)  # Teleport the player back to the center
 
     if held_keys['right mouse'] or held_keys['left mouse']:
         hand.active()
     else:
         hand.passive()
 
-    if held_keys['1']:
-        block_pick = 1
-    if held_keys['2']:
-        block_pick = 2
-    if held_keys['3']:
-        block_pick = 3
-    if held_keys['4']:
-        block_pick = 4
-
+    update_block_pick()
 
 class Voxel(Button):
     def __init__(self, position=(0, 0, 0), texture=grass_texture):
@@ -65,21 +71,26 @@ class Voxel(Button):
     def input(self, key):
         if self.hovered:
             if key == 'right mouse down':
-                if block_pick == 1:
+                placing_sound.play()
+                if block_pick == grass_texture:
                     voxel = Voxel(position=self.position +
                                   mouse.normal, texture=grass_texture)
-                if block_pick == 2:
+                if block_pick == stone_texture:
                     voxel = Voxel(position=self.position +
                                   mouse.normal, texture=stone_texture)
-                if block_pick == 3:
+                if block_pick == brick_texture:
                     voxel = Voxel(position=self.position +
                                   mouse.normal, texture=brick_texture)
-                if block_pick == 4:
+                if block_pick == dirt_texture:
                     voxel = Voxel(position=self.position +
                                   mouse.normal, texture=dirt_texture)
+                if block_pick == sand_texture:
+                    voxel = Voxel(position=self.position +
+                                  mouse.normal, texture=sand_texture)
 
             if key == 'left mouse down':
-                destroy(self)
+                    destroy(self)
+                    breaking_sound.play()
 
 
 class Sky(Entity):
@@ -109,12 +120,15 @@ class Hand(Entity):
 
     def passive(self):
         self.position = Vec2(0.4, -0.6)
-        
 
 
-for z in range(25):
-    for x in range(25):
-        voxel = Voxel(position=(x, 0, z))
+
+for z in range(-10, 10):
+    for x in range(-10, 10):
+        y = noise([x * 0.02, z * 0.02])
+        y = math.floor(y * 7.5)
+
+        voxel = Voxel(position=(x, y, z))
 
 # Define a function to close the window
 def close_window():
@@ -124,6 +138,7 @@ def close_window():
 def input(key):
     if key == 'escape':
         close_window()
+
 
 player = FirstPersonController()
 sky = Sky()
